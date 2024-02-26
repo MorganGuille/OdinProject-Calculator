0d314527751610ddd30228eeb817c52664c859ef
sarkcoder <95859784+sarkcoder@users.noreply.github.com>
2024-02-26 14:03:53 +0000

    add basic js calculator functions

--- java.js
+++ java.js
@@ -1,1 +1,44 @@
+function add(array) {
+    return array.reduce((a,b) => a + b)
+      
+  };
+  
+function subtract(array) {
+    return array.reduce((a,b) => a - b)
+      
+};
+
+function multiply(array) {
+    return array.reduce((a,b) => a * b);
+  
+};
+
+function divide(array) {
+    return array.reduce((a,b) => a / b);
+  
+};
+
+let operator = 'add';
+let firstNumber = '1';
+let secondNumber = '2';
+
+function operate(operator, firstNumber, secondNumber){
+    if (operator == 'add'){
+        return add(firstNumber, secondNumber)
+    } 
+    else if (operator == 'subtract'){
+        return subtract(firstNumber, secondNumber)
+    }
+    else if (operator == 'multiply'){
+        return multiply(firstNumber, secondNumber)
+    }
+    else if (operator == 'divide'){
+        return divide(firstNumber, secondNumber)
+    }}
+    
+console.log(operate('add', [1, 2]))
+
+
+
+let screen = document.getElementById('screenText').innerHTML = (operate('divide', [3, 2]))
 
